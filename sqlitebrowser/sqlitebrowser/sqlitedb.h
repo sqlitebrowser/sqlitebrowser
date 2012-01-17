c66580bba8d80bc5d8b81fdf52adac92972544fc
Peinthor Rene <peinthor@gmail.com>
2012-01-17 19:20:44 +0100

    fix ambiguous QString(0) init

--- sqlitebrowser/sqlitebrowser/sqlitedb.h
+++ sqlitebrowser/sqlitebrowser/sqlitedb.h
@@ -41,7 +41,7 @@ typedef QMap<int, QString> resultMap;
 class DBBrowserField
     {    
     public:
-        DBBrowserField() : name( 0 ) { }
+        DBBrowserField() : name( "" ) { }
         DBBrowserField( const QString& wname,const QString& wtype )
             : name( wname), type( wtype )
         { }
@@ -55,7 +55,7 @@ class DBBrowserField
 class DBBrowserIndex
     {    
     public:
-        DBBrowserIndex() : name( 0 ) { }
+        DBBrowserIndex() : name( "" ) { }
        DBBrowserIndex( const QString& wname,const QString& wsql )
             : name( wname), sql( wsql )
         { }
@@ -70,7 +70,7 @@ private:
 class DBBrowserTable
     {    
     public:
-        DBBrowserTable() : name( 0 ) { }
+        DBBrowserTable() : name( "" ) { }
         DBBrowserTable( const QString& wname,const QString& wsql )
             : name( wname), sql( wsql )
         { }
