e67260df031f79bedbb8866fca163740ea5aa6a8
Peinthor Rene <peinthor@gmail.com>
2013-04-20 17:13:29 +0200

    fix possible error if gen_versions.h does not exists, happens on my msys

--- src/version.sh
+++ src/version.sh
@@ -3,7 +3,12 @@ cd $1
 
 version=`git log -n1 --format="\"%h_git\""`
 version_macro="#define APP_VERSION $version"
-current_file=`cat gen_version.h`
+
+if [ -f gen_version.h ]; then
+	current_file=`cat gen_version.h`
+else
+	current_files=""
+fi
 
 if [ "$version_macro" != "$current_file" ]; then
 	echo "$version_macro" > gen_version.h
