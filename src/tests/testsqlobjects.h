457b471cf019463440ccb5da2a5ba4a0dbef3d5b
Peinthor Rene <peinthor@gmail.com>
2013-07-11 16:12:45 +0200

    split unittest class into header and cpp file

---
+++ src/tests/testsqlobjects.h
@@ -0,0 +1,16 @@
+#include <QObject>
+
+class TestTable: public QObject
+{
+    Q_OBJECT
+private slots:
+    void sqlOutput();
+    void autoincrement();
+    void notnull();
+
+    void parseSQL();
+    void parseSQLdefaultexpr();
+    void parseSQLMultiPk();
+    void parseSQLForeignKey();
+};
+
