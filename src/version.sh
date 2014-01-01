190deca0c3271fc6377ce018c2252cc7633ce9b3
Martin Kleusberg <mkleusberg@gmail.com>
2014-01-01 14:15:26 +0100

    Fix version.sh script to run on non-bash shells
    Most non-bash(like) shells don't support command line arguments for the
    echo command and print them like regular text. On these systems an
    invalid gen_version.h file is generated. This can be fixed by using
    printf instead of echo which works the same on all Unix systems.
    
    Also fix the detection whether the gen_version.h file changed before
    rewriting it. This prevents creating a file with exactly the same content
    but a newer timestamp and therefore make deciding to recompile all files
    including it.

--- src/version.sh
+++ src/version.sh
@@ -2,15 +2,15 @@
 cd $1
 
 version=`git log -n1 --format="\"%h_git\""`
-inc_guard_head="#ifndef __GEN_VERSIONS_H__\n#define __GEN_VERISONS_H__\n"
+inc_guard_head="#ifndef __GEN_VERSION_H__\n#define __GEN_VERISON_H__"
 inc_guard_foot="#endif"
-version_numbers="#define MAJOR_VERSION 999\n#define MINOR_VERSION 0\n#define PATCH_VERSION 0\n"
-version_macro="#define APP_VERSION $version\n"
+version_numbers="#define MAJOR_VERSION 999\n#define MINOR_VERSION 0\n#define PATCH_VERSION 0"
+version_macro="#define APP_VERSION $version"
 
 if [ -f gen_version.h ]; then
-	current_file=`cat gen_version.h`
+	file_unchanged=`grep "$version_macro" gen_version.h`
 fi
 
-if [ "$version_macro" != "$current_file" ]; then
-	echo -e "$inc_guard_head$version_macro$version_numbers$inc_guard_foot" > gen_version.h
+if [ "$file_unchanged" = "" ]; then
+	printf "$inc_guard_head\n$version_macro\n$version_numbers\n$inc_guard_foot\n" > gen_version.h
 fi
