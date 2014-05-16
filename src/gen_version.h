fe7a6c12270c3786e8276fb2e52f7ff5475a7dbb
Peinthor Rene <peinthor@gmail.com>
2014-05-16 18:37:35 +0200

    preprelease: bump version 3.1.0

--- src/gen_version.h
+++ src/gen_version.h
@@ -1,7 +1,7 @@
 #ifndef __GEN_VERSIONS_H__
-#define __GEN_VERISONS_H__
-#define APP_VERSION "master_git"
-#define MAJOR_VERSION 999
-#define MINOR_VERSION 0
+#define __GEN_VERSIONS_H__
+#define APP_VERSION "3.1.0"
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 1
 #define PATCH_VERSION 0
 #endif
