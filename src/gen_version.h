5c37a40522345a8b4370bbcdeecc1ac55061fe20
Sergey Muratov <van.der.msa@gmail.com>
2014-07-12 00:41:36 +0400

    Correct version

--- src/gen_version.h
+++ src/gen_version.h
@@ -1,6 +1,6 @@
-#ifndef __VERSION_H__
-#define __VERSION_H__
-#define APP_VERSION "2bd384f_git"
+#ifndef __GEN_VERSIONS_H__
+#define __GEN_VERISONS_H__
+#define APP_VERSION "master_git"
 #define MAJOR_VERSION 999
 #define MINOR_VERSION 0
 #define PATCH_VERSION 0
