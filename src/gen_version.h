1d1297734a0d53a021f442dbcb3f445241594ba7
Peinthor Rene <peinthor@gmail.com>
2015-02-08 11:24:07 +0100

    bump version to 3.5.1

--- src/gen_version.h
+++ src/gen_version.h
@@ -1,7 +1,7 @@
 #ifndef GEN_VERSION_H
 #define GEN_VERSION_H
-#define APP_VERSION "3.5.0"
+#define APP_VERSION "3.5.1"
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 5
-#define PATCH_VERSION 0
+#define PATCH_VERSION 1
 #endif
