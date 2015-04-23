sqlitebrowser/sqlitebrowser

commit dd8dd4852d97a92d6f9772dc788aaf1591d0ff19

Add Browse Table popop action to Database Structure tab
In the Database Structure tab of the main window, add a new action to
the popup menu which open upon right clicking the tree view to allow
directly changing to the Browse Data tab with the current table being
selected automatically.

See issue #246.

src/MainWindow.cpp
@@ -103,6 +103,7 @@ void MainWindow::init()
 
     // Create popup menus
     popupTableMenu = new QMenu(this);
+    popupTableMenu->addAction(ui->actionEditBrowseTable);
     popupTableMenu->addAction(ui->editModifyTableAction);
     popupTableMenu->addAction(ui->editDeleteObjectAction);
     popupTableMenu->addSeparator();
@@ -980,6 +981,7 @@ void MainWindow::changeTreeSelection()
     // Just assume first that something's selected that can not be edited at all
     ui->editDeleteObjectAction->setEnabled(false);
     ui->editModifyTableAction->setEnabled(false);
+    ui->actionEditBrowseTable->setEnabled(false);
 
     if(!ui->dbTreeWidget->currentIndex().isValid())
         return;
@@ -1004,7 +1006,11 @@ void MainWindow::changeTreeSelection()
     } else if(type == "view" || type == "trigger" || type == "index") {
         ui->editDeleteObjectAction->setEnabled(true);
     }
-    ui->actionExportCsvPopup->setEnabled(type == "table" || type == "view");
+    if(type == "table" || type == "view")
+    {
+        ui->actionEditBrowseTable->setEnabled(true);
+        ui->actionExportCsvPopup->setEnabled(true);
+    }
 }
 
 void MainWindow::openRecentFile()
@@ -2081,4 +2087,16 @@ void MainWindow::editEncryption()
         }
     }
 #endif
+}
+
+void MainWindow::switchToBrowseDataTab()
+{
+    if(!ui->dbTreeWidget->selectionModel()->hasSelection())
+        return;
+
+    QString tableToBrowse = ui->dbTreeWidget->model()->data(ui->dbTreeWidget->currentIndex().sibling(ui->dbTreeWidget->currentIndex().row(), 0)).toString();
+
+    ui->mainTab->setCurrentIndex(1);
+    ui->comboBrowseTable->setCurrentText(tableToBrowse);
+    ui->comboBrowseTable->activated(tableToBrowse);
 }

src/MainWindow.h
@@ -174,6 +174,7 @@ private slots:
     void fileAttach();
     void updateFilter(int column, const QString& value);
     void editEncryption();
+    void switchToBrowseDataTab();
 };
 
 #endif

src/MainWindow.ui
@@ -310,8 +310,8 @@
             <rect>
              <x>0</x>
              <y>0</y>
-             <width>294</width>
-             <height>444</height>
+             <width>575</width>
+             <height>458</height>
             </rect>
            </property>
            <layout class="QFormLayout" name="formLayout">
@@ -1598,6 +1598,15 @@
     <string>Save SQL file</string>
    </property>
   </action>
+  <action name="actionEditBrowseTable">
+   <property name="icon">
+    <iconset resource="icons/icons.qrc">
+     <normaloff>:/icons/table</normaloff>:/icons/table</iconset>
+   </property>
+   <property name="text">
+    <string>&amp;Browse Table</string>
+   </property>
+  </action>
  </widget>
  <customwidgets>
   <customwidget>
@@ -2469,6 +2478,22 @@
     </hint>
    </hints>
   </connection>
+  <connection>
+   <sender>actionEditBrowseTable</sender>
+   <signal>triggered()</signal>
+   <receiver>MainWindow</receiver>
+   <slot>switchToBrowseDataTab()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>-1</x>
+     <y>-1</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>499</x>
+     <y>314</y>
+    </hint>
+   </hints>
+  </connection>
  </connections>
  <slots>
   <slot>fileOpen()</slot>
@@ -2522,5 +2547,6 @@
   <slot>fileAttach()</slot>
   <slot>editEncryption()</slot>
   <slot>saveSqlFileAs()</slot>
+  <slot>switchToBrowseDataTab()</slot>
  </slots>
 </ui>

commit d6533ba5f7ec7bef2595d69a9c2d131a1d5588d8

Fix Qt4 build broken in dd8dd4852d97a92d6f9772dc788aaf1591d0ff19

src/MainWindow.cpp
@@ -2097,6 +2097,6 @@ void MainWindow::switchToBrowseDataTab()
     QString tableToBrowse = ui->dbTreeWidget->model()->data(ui->dbTreeWidget->currentIndex().sibling(ui->dbTreeWidget->currentIndex().row(), 0)).toString();
 
     ui->mainTab->setCurrentIndex(1);
-    ui->comboBrowseTable->setCurrentText(tableToBrowse);
-    ui->comboBrowseTable->activated(tableToBrowse);
+    ui->comboBrowseTable->setCurrentIndex(ui->comboBrowseTable->findText(tableToBrowse));
+    populateTable(tableToBrowse);
 }