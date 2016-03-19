2950d6500a741ddb8a5e21fc62159800687a1baa
Firat Eski <frat888@gmail.com>
2016-03-19 18:06:29 +0200

    Turkish translation

--- src/translations/sqlb_tr.ts
+++ src/translations/sqlb_tr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_GB">
+<TS version="2.0" language="tr_TR">
 <context>
     <name>AboutDialog</name>
     <message>
