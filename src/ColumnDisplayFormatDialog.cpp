c5c8106c1caa204f1b897d589b3c9507e8e694bb
jungle-boogie <sean@jungleboogie.me>
2016-04-21 09:51:12 -0700

    attempt at adding decimal (#569)
    * attempt at adding decimal
    
    hopefully a passable attempt to add decimal support per
    https://github.com/sqlitebrowser/sqlitebrowser/issues/373#issuecomment-211025161
    
    I re-organized the conversion list, too.
    
    * remove stray line
    
    * correct decimal 
    
    %d is decimal! I won't forget this now.

--- src/ColumnDisplayFormatDialog.cpp
+++ src/ColumnDisplayFormatDialog.cpp
@@ -10,16 +10,17 @@ ColumnDisplayFormatDialog::ColumnDisplayFormatDialog(const QString& colname, QSt
     // Create UI
     ui->setupUi(this);
     ui->comboDisplayFormat->addItem(tr("Default"), "default");
-    ui->comboDisplayFormat->addItem(tr("Lower case"), "lower");
-    ui->comboDisplayFormat->addItem(tr("Upper case"), "upper");
-    ui->comboDisplayFormat->addItem(tr("Unix epoch to date"), "epoch");
-    ui->comboDisplayFormat->addItem(tr("Windows DATE to date"), "winDate");
-    ui->comboDisplayFormat->addItem(tr("Julian day to date"), "julian");
-    ui->comboDisplayFormat->addItem(tr("Round number"), "round");
-    ui->comboDisplayFormat->addItem(tr("Hex number"), "hex");
-    ui->comboDisplayFormat->addItem(tr("Octal number"), "octal");
+    ui->comboDisplayFormat->addItem(tr("Decimal number"), "decimal");
     ui->comboDisplayFormat->addItem(tr("Exponent notation"), "exponent");
     ui->comboDisplayFormat->addItem(tr("Hex blob"), "hexblob");
+    ui->comboDisplayFormat->addItem(tr("Hex number"), "hex");
+    ui->comboDisplayFormat->addItem(tr("Julian day to date"), "julian");
+    ui->comboDisplayFormat->addItem(tr("Lower case"), "lower");
+    ui->comboDisplayFormat->addItem(tr("Octal number"), "octal");
+    ui->comboDisplayFormat->addItem(tr("Round number"), "round");
+    ui->comboDisplayFormat->addItem(tr("Unix epoch to date"), "epoch");
+    ui->comboDisplayFormat->addItem(tr("Upper case"), "upper");
+    ui->comboDisplayFormat->addItem(tr("Windows DATE to date"), "winDate");
     ui->labelDisplayFormat->setText(ui->labelDisplayFormat->text().arg(column_name));
 
     // Set the current format, if it's empty set the default format
@@ -80,4 +81,6 @@ void ColumnDisplayFormatDialog::updateSqlCode()
         ui->editDisplayFormat->setText("printf('%e', " + sqlb::escapeIdentifier(column_name) + ")");
     else if(format == "hexblob")
         ui->editDisplayFormat->setText("hex(" + sqlb::escapeIdentifier(column_name) + ")");
+    else if(format == "decimal")
+        ui->editDisplayFormat->setText("printf('%d', " + sqlb::escapeIdentifier(column_name) + ")");
 }
