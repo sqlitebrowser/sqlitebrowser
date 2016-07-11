5dd45967769bad76451ca372743e0e660b3455ac
M. Hasbini <mohammad.hasbini@gmail.com>
2016-07-11 18:49:41 +0300

    preparation to Arabic translation: ar_AR to ar_SA

--- src/translations/sqlb_ar.ts
+++ src/translations/sqlb_ar.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ar_AR">
+<TS version="2.0" language="ar_SA">
 <context>
     <name>AboutDialog</name>
     <message>
