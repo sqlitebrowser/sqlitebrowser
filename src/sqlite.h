8a9130883b9e89d57d33b6c3a0ee8ee59ce9957b
Justin Clift <justin@postgresql.org>
2016-08-29 15:43:20 +0100

    Update installer variables on Windows, update Qt5 path
    This should update the master branch to use "DB Browser
    for SQLite" naming, and fix the problem with the
    uninstaller not showing up in Add/Remove programs on
    Windows (issue #742)

--- src/sqlite.h
+++ src/sqlite.h
@@ -4,7 +4,11 @@
 #ifdef ENABLE_SQLCIPHER
     #define SQLITE_TEMP_STORE 2
     #define SQLITE_HAS_CODEC
+#ifdef Q_OS_WIN32
+    #include <sqlite3.h>
+#else
     #include <sqlcipher/sqlite3.h>
+#endif
 #else
     #include <sqlite3.h>
 #endif
