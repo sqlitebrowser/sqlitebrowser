sqlitebrowser/sqlitebrowser

commit 684958b29ce3d26684d9142d73df773b11189d54

Only reload DB schema when necessary
We used to reload the database schema whenever it might have been
changed and the latest version was required. For example: when the user
switches to the Structure tab we need the current schema; so we just
reloaded it to be safe - it might have been changed in the Execute SQL
tab or wherever.

With this commit we don't reload the database schema anymore when an
up-to-date version is needed but instead when it was changed by the
application. So in the example above it's not reloaded at all anymore.
Only if and when the user would execute some structure changing SQL it
would be reloaded.

Benefits:
- Better performance
- State of the structure tree view isn't lost as frequently
- Structure tree view gets updated when changing the structure in the
  Execute SQL tab

Downsides:
- Less error proof (testing!)
- No 'automatic' updates when the db is changed by an external
  application.

Any suggestions welcome!

src/sqlitedb.cpp
@@ -88,6 +88,7 @@ bool DBBrowserDB::open(const QString& db)
     if (isOpen()) close();
 
     isEncrypted = false;
+    dontCheckForStructureUpdates = false;
 
     // Get encryption settings for database file
     CipherDialog* cipher = 0;
@@ -152,6 +153,9 @@ bool DBBrowserDB::open(const QString& db)
         }
 
         curDBFilename = db;
+
+        updateSchema();
+
         return true;
     } else {
         return false;
@@ -368,9 +372,11 @@ bool DBBrowserDB::create ( const QString & db)
         // force sqlite3 do write proper file header
         // if we don't create and drop the table we might end up
         // with a 0 byte file, if the user cancels the create table dialog
+        dontCheckForStructureUpdates = true;
         executeSQL("CREATE TABLE notempty (id integer primary key);", false, false);
         executeSQL("DROP TABLE notempty;", false, false);
         executeSQL("COMMIT;", false, false);
+        dontCheckForStructureUpdates = false;
 
         // Execute default SQL
         QString default_sql = settings.value( "/db/defaultsqltext", "").toString();
@@ -380,6 +386,8 @@ bool DBBrowserDB::create ( const QString & db)
         curDBFilename = db;
         isEncrypted = false;
         isReadOnly = false;
+        dontCheckForStructureUpdates = false;
+        updateSchema();
         return true;
     } else {
         return false;
@@ -415,6 +423,7 @@ bool DBBrowserDB::close()
     objMap.clear();
     savepointList.clear();
     emit dbChanged(getDirty());
+    emit structureUpdated();
 
     // Return true to tell the calling function that the closing wasn't cancelled by the user
     return true;
@@ -590,18 +599,27 @@ bool DBBrowserDB::dump(const QString& filename,
     return false;
 }
 
-bool DBBrowserDB::executeSQL ( const QString & statement, bool dirtyDB, bool logsql)
+bool DBBrowserDB::executeSQL(QString statement, bool dirtyDB, bool logsql)
 {
     char *errmsg;
 
     if (!isOpen())
         return false;
 
+    statement = statement.trimmed();
+
     if (logsql) logSQL(statement, kLogMsg_App);
     if (dirtyDB) setSavepoint();
 
     if (SQLITE_OK == sqlite3_exec(_db, statement.toUtf8(), NULL, NULL, &errmsg))
     {
+        // Update DB structure after executing an SQL statement. But try to avoid doing unnecessary updates.
+        if(!dontCheckForStructureUpdates && (statement.startsWith("ALTER", Qt::CaseInsensitive) ||
+                statement.startsWith("CREATE", Qt::CaseInsensitive) ||
+                statement.startsWith("DROP", Qt::CaseInsensitive) ||
+                statement.startsWith("ROLLBACK", Qt::CaseInsensitive)))
+            updateSchema();
+
         return true;
     } else {
         lastErrorMessage = QString("%1 (%2)").arg(QString::fromUtf8(errmsg)).arg(statement);
@@ -616,7 +634,7 @@ bool DBBrowserDB::executeMultiSQL(const QString& statement, bool dirty, bool log
     if(!isOpen())
         return false;
 
-    QString query = statement;
+    QString query = statement.trimmed();
 
     query.remove(QRegExp("^\\s*BEGIN TRANSACTION;|COMMIT;\\s*$"));
 
@@ -641,6 +659,7 @@ bool DBBrowserDB::executeMultiSQL(const QString& statement, bool dirty, bool log
     const char *tail = utf8Query.data();
     int res = 0;
     unsigned int line = 0;
+    bool structure_updated = false;
     do
     {
         line++;
@@ -654,6 +673,14 @@ bool DBBrowserDB::executeMultiSQL(const QString& statement, bool dirty, bool log
             lastErrorMessage = tr("Action cancelled.");
             return false;
         }
+
+        // Check whether the DB structure is changed by this statement
+        QString qtail = QString(tail);
+        if(!dontCheckForStructureUpdates && !structure_updated && (qtail.startsWith("ALTER", Qt::CaseInsensitive) ||
+                qtail.startsWith("CREATE", Qt::CaseInsensitive) ||
+                qtail.startsWith("DROP", Qt::CaseInsensitive) ||
+                qtail.startsWith("ROLLBACK", Qt::CaseInsensitive)))
+            structure_updated = true;
 
         // Execute next statement
         res = sqlite3_prepare_v2(_db, tail, tail_length, &vm, &tail);
@@ -676,6 +703,10 @@ bool DBBrowserDB::executeMultiSQL(const QString& statement, bool dirty, bool log
             return false;
         }
     } while(tail && *tail != 0 && (res == SQLITE_OK || res == SQLITE_DONE));
+
+    // If the DB structure was changed by some command in this SQL script, update our schema representations
+    if(structure_updated)
+        updateSchema();
 
     // Exit
     return true;
@@ -904,9 +935,7 @@ bool DBBrowserDB::createTable(const QString& name, const sqlb::FieldVector& stru
         table.addField(structure.at(i));
 
     // Execute it and update the schema
-    bool result = executeSQL(table.sql());
-    updateSchema();
-    return result;
+    return executeSQL(table.sql());
 }
 
 bool DBBrowserDB::addColumn(const QString& tablename, const sqlb::FieldPtr& field)
@@ -914,9 +943,7 @@ bool DBBrowserDB::addColumn(const QString& tablename, const sqlb::FieldPtr& fiel
     QString sql = QString("ALTER TABLE %1 ADD COLUMN %2").arg(sqlb::escapeIdentifier(tablename)).arg(field->toString());
 
     // Execute it and update the schema
-    bool result = executeSQL(sql);
-    updateSchema();
-    return result;
+    return executeSQL(sql);
 }
 
 bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sqlb::FieldPtr to, int move)
@@ -996,12 +1023,14 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     }
 
     // Create the new table
-    if(!executeSQL(newSchema.sql()))
+    dontCheckForStructureUpdates = true;
+    if(!executeSQL(newSchema.sql(), true, true))
     {
         QString error(tr("renameColumn: creating new table failed. DB says: %1").arg(lastErrorMessage));
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
+        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1012,6 +1041,7 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
+        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1029,12 +1059,13 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     setPragma("foreign_keys", "0");
 
     // Delete the old table
-    if(!executeSQL(QString("DROP TABLE %1;").arg(sqlb::escapeIdentifier(tablename))))
+    if(!executeSQL(QString("DROP TABLE %1;").arg(sqlb::escapeIdentifier(tablename)), true, true))
     {
         QString error(tr("renameColumn: deleting old table failed. DB says: %1").arg(lastErrorMessage));
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
+        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1042,6 +1073,7 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     if(!renameTable("sqlitebrowser_rename_column_new_table", tablename))
     {
         revertToSavepoint("sqlitebrowser_rename_column");
+        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1062,10 +1094,12 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     {
         lastErrorMessage = tr("renameColumn: releasing savepoint failed. DB says: %1").arg(lastErrorMessage);
         qWarning() << lastErrorMessage;
+        dontCheckForStructureUpdates = false;
         return false;
     }
 
     // Success, update the DB schema before returning
+    dontCheckForStructureUpdates = true;
     updateSchema();
     return true;
 }
@@ -1081,7 +1115,6 @@ bool DBBrowserDB::renameTable(const QString& from_table, const QString& to_table
         qWarning() << lastErrorMessage;
         return false;
     } else {
-        updateSchema();
         return true;
     }
 }
@@ -1200,6 +1233,8 @@ void DBBrowserDB::updateSchema( )
             }
         }
     }
+
+    emit structureUpdated();
 }
 
 QString DBBrowserDB::getPragma(const QString& pragma)

src/sqlitedb.h
@@ -43,7 +43,7 @@ class DBBrowserDB : public QObject
     Q_OBJECT
 
 public:
-    explicit DBBrowserDB () : _db(0), isEncrypted(false), isReadOnly(false) {}
+    explicit DBBrowserDB () : _db(0), isEncrypted(false), isReadOnly(false), dontCheckForStructureUpdates(false) {}
     virtual ~DBBrowserDB (){}
     bool open ( const QString & db);
     bool attach(const QString& filename, QString attach_as = "");
@@ -55,7 +55,7 @@ public:
     bool releaseAllSavepoints();
     bool revertAll();
     bool dump(const QString & filename, const QStringList &tablesToDump, bool insertColNames, bool insertNew, bool exportSchema, bool exportData);
-    bool executeSQL ( const QString & statement, bool dirtyDB=true, bool logsql=true);
+    bool executeSQL(QString statement, bool dirtyDB = true, bool logsql = true);
     bool executeMultiSQL(const QString& statement, bool dirty = true, bool log = false);
 
     /**
@@ -127,6 +127,7 @@ public:
 signals:
     void sqlExecuted(QString sql, int msgtype);
     void dbChanged(bool dirty);
+    void structureUpdated();
 
 private:
     QString curDBFilename;
@@ -137,6 +138,8 @@ private:
     bool isReadOnly;
 
     bool tryEncryptionSettings(const QString& filename, bool* encrypted, CipherDialog*& cipherSettings);
+
+    bool dontCheckForStructureUpdates;
 };
 
 #endif

src/EditTableDialog.cpp
@@ -166,7 +166,6 @@ void EditTableDialog::reject()
 {    
     // Then rollback to our savepoint
     pdb->revertToSavepoint(m_sRestorePointName);
-    pdb->updateSchema();
 
     QDialog::reject();
 }

src/MainWindow.cpp
@@ -78,6 +78,7 @@ void MainWindow::init()
     // Connect SQL logging and database state setting to main window
     connect(&db, SIGNAL(dbChanged(bool)), this, SLOT(dbState(bool)));
     connect(&db, SIGNAL(sqlExecuted(QString, int)), this, SLOT(logSql(QString,int)));
+    connect(&db, SIGNAL(structureUpdated()), this, SLOT(populateStructure()));
 
     // Set the validator for the goto line edit
     ui->editGoto->setValidator(gotoValidator);
@@ -294,7 +295,6 @@ bool MainWindow::fileOpen(const QString& fileName, bool dontAddToRecentFiles)
                     addToRecentFilesMenu(wFile);
                 openSqlTab(true);
                 loadExtensionsFromSettings();
-                populateStructure();
                 if(ui->mainTab->currentIndex() == BrowseTab)
                     populateTable();
                 else if(ui->mainTab->currentIndex() == PragmaTab)
@@ -326,7 +326,6 @@ void MainWindow::fileNew()
         statusEncryptionLabel->setVisible(false);
         statusReadOnlyLabel->setVisible(false);
         loadExtensionsFromSettings();
-        populateStructure();
         populateTable();
         openSqlTab(true);
         createTable();
@@ -338,7 +337,6 @@ void MainWindow::populateStructure()
     QString old_table = ui->comboBrowseTable->currentText();
 
     // Refresh the structure tab
-    db.updateSchema();
     dbStructureModel->reloadData();
     ui->dbTreeWidget->setRootIndex(dbStructureModel->index(1, 0));      // Show the 'All' part of the db structure
     ui->dbTreeWidget->expandToDepth(0);
@@ -513,7 +511,6 @@ bool MainWindow::fileClose()
         return false;
 
     setWindowTitle(QApplication::applicationName());
-    populateStructure();
     loadPragmas();
     statusEncryptionLabel->setVisible(false);
     statusReadOnlyLabel->setVisible(false);
@@ -708,7 +705,6 @@ void MainWindow::createTable()
     EditTableDialog dialog(&db, "", true, this);
     if(dialog.exec())
     {
-        populateStructure();
         populateTable();
     }
 }
@@ -721,10 +717,7 @@ void MainWindow::createIndex()
     }
 
     CreateIndexDialog dialog(&db, this);
-    if(dialog.exec())
-    {
-        populateStructure();
-    }
+    dialog.exec();
 }
 
 void MainWindow::compact()
@@ -750,7 +743,6 @@ void MainWindow::deleteObject()
             QString error = tr("Error: could not delete the %1. Message from database engine:\n%2").arg(type).arg(db.lastErrorMessage);
             QMessageBox::warning(this, QApplication::applicationName(), error);
         } else {
-            populateStructure();
             populateTable();
             changeTreeSelection();
         }
@@ -770,10 +762,7 @@ void MainWindow::editTable()
 
     EditTableDialog dialog(&db, tableToEdit, false, this);
     if(dialog.exec())
-    {
-        populateStructure();
         populateTable();
-    }
 }
 
 void MainWindow::helpWhatsThis()
@@ -885,7 +874,7 @@ void MainWindow::executeQuery()
     if (query.isEmpty())
         return;
 
-    query = query.remove(QRegExp("^\\s*BEGIN TRANSACTION;|COMMIT;\\s*$"));
+    query = query.remove(QRegExp("^\\s*BEGIN TRANSACTION;|COMMIT;\\s*$")).trimmed();
 
     //log the query
     db.logSQL(query, kLogMsg_User);
@@ -897,6 +886,7 @@ void MainWindow::executeQuery()
     QString statusMessage;
     bool modified = false;
     bool wasdirty = db.getDirty();
+    bool structure_updated = false;
 
     // there is no choice, we have to start a transaction before
     // we create the prepared statement, otherwise every executed
@@ -912,6 +902,15 @@ void MainWindow::executeQuery()
     timer.start();
     do
     {
+        // Check whether the DB structure is changed by this statement
+        QString qtail = QString(tail);
+        if(!structure_updated && (qtail.startsWith("ALTER", Qt::CaseInsensitive) ||
+                qtail.startsWith("CREATE", Qt::CaseInsensitive) ||
+                qtail.startsWith("DROP", Qt::CaseInsensitive) ||
+                qtail.startsWith("ROLLBACK", Qt::CaseInsensitive)))
+            structure_updated = true;
+
+        // Execute next statement
         int tail_length_before = tail_length;
         const char* qbegin = tail;
         sql3status = sqlite3_prepare_v2(db._db,tail, tail_length, &vm, &tail);
@@ -996,6 +995,10 @@ void MainWindow::executeQuery()
 
     if(!modified && !wasdirty)
         db.revertToSavepoint(); // better rollback, if the logic is not enough we can tune it.
+
+    // If the DB structure was changed by some command in this SQL script, update our schema representations
+    if(structure_updated)
+        db.updateSchema();
 }
 
 void MainWindow::mainTabSelected(int tabindex)
@@ -1005,12 +1008,10 @@ void MainWindow::mainTabSelected(int tabindex)
     switch (tabindex)
     {
     case StructureTab:
-        populateStructure();
         break;
 
     case BrowseTab:
         m_currentTabTableModel = m_browseTableModel;
-        populateStructure();
         populateTable();
         break;
 
@@ -1046,7 +1047,6 @@ void MainWindow::importTableFromCSV()
         ImportCsvDialog dialog(wFile, &db, this);
         if(dialog.exec())
         {
-            populateStructure();
             populateTable();
             QMessageBox::information(this, QApplication::applicationName(), tr("Import completed"));
         }
@@ -1108,7 +1108,6 @@ void MainWindow::fileRevert()
         if(QMessageBox::question(this, QApplication::applicationName(), msg, QMessageBox::Yes | QMessageBox::Default, QMessageBox::No | QMessageBox::Escape) == QMessageBox::Yes)
         {
             db.revertAll();
-            populateStructure();
             populateTable();
         }
     }
@@ -1176,7 +1175,7 @@ void MainWindow::importDatabaseFromSQL()
     {
         fileOpen(newDbFile);
     } else {
-        populateStructure();
+        db.updateSchema();
         populateTable();
     }
 }

src/MainWindow.h
@@ -162,12 +162,12 @@ public slots:
     void browseRefresh();
     void jumpToRow(const QString& table, QString column, const QByteArray& value);
     void switchToBrowseDataTab(QString tableToBrowse = QString());
+    void populateStructure();
 
 private slots:
     void createTreeContextMenu(const QPoint & qPoint);
     void changeTreeSelection();
     void fileNew();
-    void populateStructure();
     void populateTable();
     void clearTableBrowser();
     bool fileClose();

commit e7b12332f7935b75a0f972cbb6d04e3587800898

Fix bug introduced in 684958b29ce3d26684d9142d73df773b11189d54
This would stop the db schema from being reloaded when it actually
should be.

Also simplify code.

src/sqlitedb.cpp
@@ -372,11 +372,12 @@ bool DBBrowserDB::create ( const QString & db)
         // force sqlite3 do write proper file header
         // if we don't create and drop the table we might end up
         // with a 0 byte file, if the user cancels the create table dialog
-        dontCheckForStructureUpdates = true;
-        executeSQL("CREATE TABLE notempty (id integer primary key);", false, false);
-        executeSQL("DROP TABLE notempty;", false, false);
-        executeSQL("COMMIT;", false, false);
-        dontCheckForStructureUpdates = false;
+        {
+            NoStructureUpdateChecks nup(*this);
+            executeSQL("CREATE TABLE notempty (id integer primary key);", false, false);
+            executeSQL("DROP TABLE notempty;", false, false);
+            executeSQL("COMMIT;", false, false);
+        }
 
         // Execute default SQL
         QString default_sql = settings.value( "/db/defaultsqltext", "").toString();
@@ -386,7 +387,6 @@ bool DBBrowserDB::create ( const QString & db)
         curDBFilename = db;
         isEncrypted = false;
         isReadOnly = false;
-        dontCheckForStructureUpdates = false;
         updateSchema();
         return true;
     } else {
@@ -1023,14 +1023,13 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     }
 
     // Create the new table
-    dontCheckForStructureUpdates = true;
+    NoStructureUpdateChecks nup(*this);
     if(!executeSQL(newSchema.sql(), true, true))
     {
         QString error(tr("renameColumn: creating new table failed. DB says: %1").arg(lastErrorMessage));
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
-        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1041,7 +1040,6 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
-        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1065,7 +1063,6 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
         qWarning() << error;
         revertToSavepoint("sqlitebrowser_rename_column");
         lastErrorMessage = error;
-        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1073,7 +1070,6 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     if(!renameTable("sqlitebrowser_rename_column_new_table", tablename))
     {
         revertToSavepoint("sqlitebrowser_rename_column");
-        dontCheckForStructureUpdates = false;
         return false;
     }
 
@@ -1094,12 +1090,10 @@ bool DBBrowserDB::renameColumn(const QString& tablename, const QString& name, sq
     {
         lastErrorMessage = tr("renameColumn: releasing savepoint failed. DB says: %1").arg(lastErrorMessage);
         qWarning() << lastErrorMessage;
-        dontCheckForStructureUpdates = false;
         return false;
     }
 
     // Success, update the DB schema before returning
-    dontCheckForStructureUpdates = true;
     updateSchema();
     return true;
 }

src/sqlitedb.h
@@ -140,6 +140,16 @@ private:
     bool tryEncryptionSettings(const QString& filename, bool* encrypted, CipherDialog*& cipherSettings);
 
     bool dontCheckForStructureUpdates;
+
+    class NoStructureUpdateChecks
+    {
+    public:
+        NoStructureUpdateChecks(DBBrowserDB& db) : m_db(db) { m_db.dontCheckForStructureUpdates = true; }
+        ~NoStructureUpdateChecks() { m_db.dontCheckForStructureUpdates = false; }
+
+    private:
+          DBBrowserDB& m_db;
+    };
 };
 
 #endif