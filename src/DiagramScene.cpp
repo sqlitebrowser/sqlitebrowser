cbe1d1baaaf55846b189f86877c003a4a93a75c7
Vladislav Tronko <innermous@gmail.com>
2016-12-29 08:03:10 +0200

    Make TableWidget moveable
    We use subclassed proxy widget, that overrides overridden mouse event methods
    and just returns default scene mouse events.

--- src/DiagramScene.cpp
+++ src/DiagramScene.cpp
@@ -7,6 +7,29 @@
 #include <QGraphicsProxyWidget>
 #include <QDebug>
 
+class MovableProxyWidget : public QGraphicsProxyWidget
+{
+    Q_OBJECT
+
+#define OVERRIDE_MOUSE_EVENT(event_type) \
+    void event_type(QGraphicsSceneMouseEvent* event) Q_DECL_OVERRIDE \
+    { return QGraphicsItem::event_type(event); }
+
+public:
+    MovableProxyWidget(QGraphicsItem* parent = Q_NULLPTR)
+        : QGraphicsProxyWidget(parent)
+    {
+        setFlags(flags() | ItemIsMovable | ItemIsSelectable);
+    }
+
+    ~MovableProxyWidget() { }
+
+    OVERRIDE_MOUSE_EVENT(mouseDoubleClickEvent)
+    OVERRIDE_MOUSE_EVENT(mouseMoveEvent)
+    OVERRIDE_MOUSE_EVENT(mousePressEvent)
+    OVERRIDE_MOUSE_EVENT(mouseReleaseEvent)
+};
+
 DiagramScene::DiagramScene(const DBBrowserDB& db, QWidget *parent)
     : QGraphicsScene(parent)
     , m_db(db)
@@ -36,5 +59,9 @@ void DiagramScene::addTable(const QString& tableName)
     TableWidget* widget = new TableWidget(model);
     m_tables.push_back(widget);
 
-    addWidget(widget);
+    MovableProxyWidget* proxy = new MovableProxyWidget;
+    proxy->setWidget(widget);
+    addItem(proxy);
 }
+
+#include "DiagramScene.moc"
