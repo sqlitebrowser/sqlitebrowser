sqlitebrowser/sqlitebrowser

commit 194979edb049a62c373f177ed68056a011c00faa

Slight optimization of loop in populateTable() method

src/MainWindow.cpp
@@ -429,7 +429,8 @@ void MainWindow::populateTable()
     } else {
         QVector<QString> v;
         bool only_defaults = true;
-        for(int i=0;i<db.getObjectByName(tablename).table.fields().size();i++)
+        const sqlb::FieldVector& tablefields = db.getObjectByName(tablename).table.fields();
+        for(int i=0; tablefields.size(); ++i)
         {
             QString format = tableIt.value().displayFormats[i+1];
             if(format.size())
@@ -437,7 +438,7 @@ void MainWindow::populateTable()
                 v.push_back(format);
                 only_defaults = false;
             } else {
-                v.push_back(sqlb::escapeIdentifier(db.getObjectByName(tablename).table.fields().at(i)->name()));
+                v.push_back(sqlb::escapeIdentifier(tablefields.at(i)->name()));
             }
         }
         if(only_defaults)

commit e3ef13a916e97d8a106b4ff8ed033d634dbc0500

Fix bug introduced by 194979edb049a62c373f177ed68056a011c00faa

src/MainWindow.cpp
@@ -430,7 +430,7 @@ void MainWindow::populateTable()
         QVector<QString> v;
         bool only_defaults = true;
         const sqlb::FieldVector& tablefields = db.getObjectByName(tablename).table.fields();
-        for(int i=0; tablefields.size(); ++i)
+        for(int i=0; i<tablefields.size(); ++i)
         {
             QString format = tableIt.value().displayFormats[i+1];
             if(format.size())