3ff686d96ff07a28c6de188c72e3104b9d80be67
Martin Kleusberg <mkleusberg@gmail.com>
2017-05-03 21:11:29 +0200

    Show consistent slashes in recent files menu actions
    See issue #735.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1334,7 +1334,7 @@ void MainWindow::updateRecentFileActions()
     int numRecentFiles = qMin(files.size(), (int)MaxRecentFiles);
 
     for (int i = 0; i < numRecentFiles; ++i) {
-        QString text = tr("&%1 %2").arg(i + 1).arg(files[i]);
+        QString text = tr("&%1 %2").arg(i + 1).arg(QDir::toNativeSeparators(files[i]));
         recentFileActs[i]->setText(text);
         recentFileActs[i]->setData(files[i]);
         recentFileActs[i]->setVisible(true);
