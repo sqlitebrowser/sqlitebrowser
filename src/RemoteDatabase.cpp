f830f4265cdffbc1a03e995e0285f1783ce2501e
Martin Kleusberg <mkleusberg@gmail.com>
2017-09-01 12:36:20 +0200

    dbhub: Fix progress dialog for very large files
    See issue #1095.

--- src/RemoteDatabase.cpp
+++ src/RemoteDatabase.cpp
@@ -238,9 +238,14 @@ void RemoteDatabase::updateProgress(qint64 bytesTransmitted, qint64 bytesTotal)
         m_progress->reset();
     } else {
         // It's still downloading and we know the current progress
+
+        // Were using a range 0 to 10000 here, the progress dialog will calculate 0% to 100% values from that. The reason we're not using
+        // the byte counts as-is is that they're 64bit wide while the progress dialog takes only 32bit values, so for large files the values
+        // would lose precision. The reason why we're not using a range 0 to 100 is that our range increases the precision a bit and this way
+        // we're prepared if the progress dialog will show decimal numbers one day on one platform.
         m_progress->setMinimum(0);
-        m_progress->setMaximum(bytesTotal);
-        m_progress->setValue(bytesTransmitted);
+        m_progress->setMaximum(10000);
+        m_progress->setValue(static_cast<int>((static_cast<float>(bytesTransmitted) / static_cast<float>(bytesTotal)) * 10000.0f));
     }
 
     // Check if the Cancel button has been pressed
