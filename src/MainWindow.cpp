a457d29fe26160c971b69f4ffaaeee6bf8927aaf
mgrojo <mgrojo@gmail.com>
2017-11-19 01:11:20 +0100

    Fix one case for the error visual hint in the SQL results pane

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1122,6 +1122,7 @@ void MainWindow::executeQuery()
                 // The query takes the last placeholder as it may itself contain the sequence '%' + number
                 statusMessage = tr("%1 rows returned in %2ms from: %3").arg(
                             sqlWidget->getModel()->totalRowCount()).arg(timer.elapsed()).arg(queryPart.trimmed());
+                ok = true;
                 ui->actionSqlResultsSave->setEnabled(true);
                 ui->actionSqlResultsSaveAsView->setEnabled(!db.readOnly());
 
