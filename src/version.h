242298fed62b8598796d7af7167a1b87232721b9
Karim ElDeeb <keldeeb@gmail.com>
2018-05-20 14:36:51 +0200

    Change ProductVersion to be same as FileVersion

--- src/version.h
+++ src/version.h
@@ -6,18 +6,11 @@
 
 #define str(s) #s
 #define xstr(s) str(s)
+#define APP_VERSION xstr(MAJOR_VERSION) "." xstr(MINOR_VERSION) "." xstr(PATCH_VERSION)
 
-// It is required in 'winapp.rc' and always use the Major.Minor.Patch format.
-#define PRODUCT_VERSION xstr(MAJOR_VERSION) "." xstr(MINOR_VERSION) "." xstr(PATCH_VERSION)
-
-// If BUILD_VERSION is defined by the compiler, then it is a nightly build, and APP_VERSION
-// will use the Major.Minor.Patch.Build format, otherwise it is Major.Minor.Patch only.
-// If 'BUILD_VERSION == 0' then it is a stable verion.
+// If it is defined by the compiler, then it is a nightly build, and in the YYYYMMDD format.
 #ifndef BUILD_VERSION
     #define BUILD_VERSION 0
-    #define APP_VERSION PRODUCT_VERSION
-#else
-    #define APP_VERSION PRODUCT_VERSION "." xstr(BUILD_VERSION)
 #endif
 
 #endif
