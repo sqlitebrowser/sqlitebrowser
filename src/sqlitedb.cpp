sqlitebrowser/sqlitebrowser

commit 94bbb4602910e586a2c15076a7c1ee02bdbd6206

Add automatic crypted databases open via dotenvs
This adds support for `.env` files next to the crypted databases that
are to be opened that contains the needed cipher settings.

The only required one is the plain-text password as a value for the key
with the name of the database like this:

    myCryptedDatabase.sqlite = MyPassword

This way, databases with a different extension are supported too:

    myCryptedDatabase.db = MyPassword

You can also specify a custom page size adding a different line
(anywhere in the file) like this:

    myCryptedDatabase.db_pageSize = 2048

If not specified, `1024` is used.

You can also specify the format of the specified key using the
associated integer id:

    anotherCryptedDatabase.sqlite = 0xCAFEBABE
    anotherCryptedDatabase.sqlite_keyFormat = 1

where `1` means a Raw key. If not specified, `0` is used, which means a
simple text Passphrase.

Dotenv files (`.env`) are already used on other platforms and by
different tools to manage environment variables, and it's recommended
to be ignored from version control systems, so they won't leak.

src/sqlitedb.cpp
@@ -261,6 +261,11 @@ bool DBBrowserDB::tryEncryptionSettings(const QString& filePath, bool* encrypted
         return false;
 
     // Try reading from database
+
+#ifdef ENABLE_SQLCIPHER
+    bool isDotenvChecked = false;
+#endif
+
     *encrypted = false;
     cipherSettings = nullptr;
     while(true)
@@ -281,6 +286,61 @@ bool DBBrowserDB::tryEncryptionSettings(const QString& filePath, bool* encrypted
         {
             sqlite3_finalize(vm);
 #ifdef ENABLE_SQLCIPHER
+            // Being in a while loop, we don't want to check the same file multiple times
+            if (!isDotenvChecked) {
+                QFile databaseFile(filePath);
+                QFileInfo databaseFileInfo(databaseFile);
+
+                QString databaseDirectoryPath = databaseFileInfo.dir().path();
+                QString databaseFileName(databaseFileInfo.fileName());
+
+                QString dotenvFilePath = databaseDirectoryPath + "/.env";
+                QSettings::Format dotenvFormat = Settings::getDotenvFormat();
+                QSettings dotenv(dotenvFilePath, dotenvFormat);
+
+                QVariant passwordValue = dotenv.value(databaseFileName);
+
+                bool foundPassword = !passwordValue.isNull();
+
+                isDotenvChecked = true;
+
+                if (foundPassword)
+                {
+                    QString password = passwordValue.toString();
+
+                    QVariant keyFormatValue = dotenv.value(databaseFileName + "_keyFormat", QVariant(CipherSettings::KeyFormats::Passphrase));
+                    CipherSettings::KeyFormats keyFormat = CipherSettings::getKeyFormat(keyFormatValue.toInt());
+
+                    QVariant pageSizeValue = dotenv.value(databaseFileName + "_pageSize", QVariant(CipherSettings::defaultPageSize));
+                    int pageSize = pageSizeValue.toInt();
+
+                    cipherSettings = new CipherSettings();
+
+                    cipherSettings->setKeyFormat(keyFormat);
+                    cipherSettings->setPassword(password);
+                    cipherSettings->setPageSize(pageSize);
+
+                    // Close and reopen database first to be in a clean state after the failed read attempt from above
+                    sqlite3_close(dbHandle);
+                    if(sqlite3_open_v2(filePath.toUtf8(), &dbHandle, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK)
+                    {
+                        return false;
+                    }
+
+                    // Set the key
+                    sqlite3_exec(dbHandle, QString("PRAGMA key = %1").arg(cipherSettings->getPassword()).toUtf8(), nullptr, nullptr, nullptr);
+
+                    // Set the page size if it differs from the default value
+                    if(cipherSettings->getPageSize() != CipherSettings::defaultPageSize)
+                        sqlite3_exec(dbHandle, QString("PRAGMA cipher_page_size = %1;").arg(cipherSettings->getPageSize()).toUtf8(), nullptr, nullptr, nullptr);
+
+                    *encrypted = true;
+
+                    // Skip the CipherDialog prompt for now to test if the saved password was correct
+                    continue;
+                }
+            }
+
             CipherDialog *cipherDialog = new CipherDialog(nullptr, false);
             if(cipherDialog->exec())
             {

commit 58e6677e8491adac5713c90eb522c0293950b3d3

Fix memory leaks
(introduced by 94bbb46)

src/sqlitedb.cpp
@@ -315,6 +315,7 @@ bool DBBrowserDB::tryEncryptionSettings(const QString& filePath, bool* encrypted
                     QVariant pageSizeValue = dotenv.value(databaseFileName + "_pageSize", QVariant(CipherSettings::defaultPageSize));
                     int pageSize = pageSizeValue.toInt();
 
+                    delete cipherSettings;
                     cipherSettings = new CipherSettings();
 
                     cipherSettings->setKeyFormat(keyFormat);
@@ -325,6 +326,8 @@ bool DBBrowserDB::tryEncryptionSettings(const QString& filePath, bool* encrypted
                     sqlite3_close(dbHandle);
                     if(sqlite3_open_v2(filePath.toUtf8(), &dbHandle, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK)
                     {
+	                    delete cipherSettings;
+	                    cipherSettings = nullptr;
                         return false;
                     }
 