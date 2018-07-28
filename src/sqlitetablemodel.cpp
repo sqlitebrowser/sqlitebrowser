sqlitebrowser/sqlitebrowser

commit 3da520cdd1de73b09c32fe2aad08e2c3d577550b

Clean up multi threading patch, fix build and some bugs
Make strings translatable, remove some more debug code, fix tests,
reduce size of patch slightly, remove weird tooltip, don't crash when
closing database, simplify code, fix filters, don't link agains pthread
on Windows.

src/sqlitetablemodel.cpp
@@ -1,4 +1,3 @@
-
 #include "sqlitetablemodel.h"
 #include "sqlitedb.h"
 #include "sqlite.h"
@@ -24,7 +23,7 @@ SqliteTableModel::SqliteTableModel(DBBrowserDB& db, QObject* parent, size_t chun
     , m_encoding(encoding)
 {
     worker = new RowLoader(
-        [this](){ return m_db.get("reading rows"); },
+        [this](){ return m_db.get(tr("reading rows")); },
         [this](QString stmt){ return m_db.logSQL(stmt, kLogMsg_App); },
         m_headers, m_mutexDataCache, m_cache
         );
@@ -95,8 +94,6 @@ void SqliteTableModel::handleRowCountComplete (int life_id, int num_rows)
 
 void SqliteTableModel::reset()
 {
-    //std::cout << "\n\nSqliteTableModel::reset()\n";
-
     clearCache();
 
     m_sTable.clear();
@@ -249,9 +246,7 @@ QVariant SqliteTableModel::data(const QModelIndex &index, int role) const
     {
         cached_row = &m_cache.at(index.row());
         row_available = true;
-    }
-    else
-    {
+    } else {
         blank_data = makeDefaultCacheEntry();
         cached_row = &blank_data;
         row_available = false;
@@ -260,17 +255,13 @@ QVariant SqliteTableModel::data(const QModelIndex &index, int role) const
     if(role == Qt::DisplayRole || role == Qt::EditRole)
     {
         if(!row_available)
-            return decode("loading...");
+            return tr("loading...");
         if(role == Qt::DisplayRole && cached_row->at(index.column()).isNull())
         {
             return Settings::getValue("databrowser", "null_text").toString();
-        }
-        else if(role == Qt::DisplayRole && nosync_isBinary(index))
-        {
+        } else if(role == Qt::DisplayRole && nosync_isBinary(index)) {
             return Settings::getValue("databrowser", "blob_text").toString();
-        }
-        else if(role == Qt::DisplayRole)
-        {
+        } else if(role == Qt::DisplayRole) {
             int limit = Settings::getValue("databrowser", "symbol_limit").toInt();
             QByteArray displayText = cached_row->at(index.column());
             if (displayText.length() > limit) {
@@ -282,16 +273,12 @@ QVariant SqliteTableModel::data(const QModelIndex &index, int role) const
         } else {
             return decode(cached_row->at(index.column()));
         }
-    }
-    else if(role == Qt::FontRole)
-    {
+    } else if(role == Qt::FontRole) {
         QFont font;
         if(!row_available || cached_row->at(index.column()).isNull() || nosync_isBinary(index))
             font.setItalic(true);
         return font;
-    }
-    else if(role == Qt::ForegroundRole)
-    {
+    } else if(role == Qt::ForegroundRole) {
         if(!row_available)
             return QColor(100, 100, 100);
         if(cached_row->at(index.column()).isNull())
@@ -299,9 +286,7 @@ QVariant SqliteTableModel::data(const QModelIndex &index, int role) const
         else if (nosync_isBinary(index))
             return QColor(Settings::getValue("databrowser", "bin_fg_colour").toString());
         return QColor(Settings::getValue("databrowser", "reg_fg_colour").toString());
-    }
-    else if (role == Qt::BackgroundRole)
-    {
+    } else if (role == Qt::BackgroundRole) {
         if(!row_available)
             return QColor(255, 200, 200);
         if(cached_row->at(index.column()).isNull())
@@ -309,9 +294,7 @@ QVariant SqliteTableModel::data(const QModelIndex &index, int role) const
         else if (nosync_isBinary(index))
             return QColor(Settings::getValue("databrowser", "bin_bg_colour").toString());
         return QColor(Settings::getValue("databrowser", "reg_bg_colour").toString());
-    }
-    else if(role == Qt::ToolTipRole)
-    {
+    } else if(role == Qt::ToolTipRole) {
         sqlb::ForeignKeyClause fk = getForeignKeyClause(index.column()-1);
         if(fk.isSet())
             return tr("References %1(%2)\nHold Ctrl+Shift and click to jump there").arg(fk.table()).arg(fk.columns().join(","));
@@ -503,7 +486,6 @@ bool SqliteTableModel::insertRows(int row, int count, const QModelIndex& parent)
     beginInsertRows(parent, row, row + count - 1);
     for(unsigned int i = 0; i < tempList.size(); ++i)
     {
-        //std::cout << "inserting at " << i + row << std::endl;
         m_cache.insert(i + row, std::move(tempList.at(i)));
         m_currentRowCount++;
     }
@@ -691,7 +673,7 @@ void SqliteTableModel::removeCommentsFromQuery(QString& query)
 QStringList SqliteTableModel::getColumns(std::shared_ptr<sqlite3> pDb, const QString& sQuery, QVector<int>& fieldsTypes)
 {
     if(!pDb)
-        pDb = m_db.get("retrieving list of columns");
+        pDb = m_db.get(tr("retrieving list of columns"));
 
     sqlite3_stmt* stmt;
     QByteArray utf8Query = sQuery.toUtf8();
@@ -945,11 +927,8 @@ void SqliteTableModel::triggerCacheLoad (int row) const
     QMutexLocker lk(&m_mutexDataCache);
     m_cache.smallestNonAvailableRange(row_begin, row_end);
 
-    if(row_end != row_begin) {
+    if(row_end != row_begin)
         worker->triggerFetch(m_lifeCounter, row_begin, row_end);
-    } else {
-        //std::cout << "entire range already loaded\n";
-    }
 }
 
 void SqliteTableModel::triggerCacheLoad (int row_begin, int row_end) const

CMakeLists.txt
@@ -78,8 +78,6 @@ endif()
 add_subdirectory(${QHEXEDIT_DIR})
 add_subdirectory(${QCUSTOMPLOT_DIR})
 
-add_subdirectory(/home/michael/dev/gtest/git/googletest /home/michael/dev/gtest/build)
-
 find_package(Qt5Widgets REQUIRED)
 find_package(Qt5LinguistTools REQUIRED)
 find_package(Qt5Network REQUIRED)
@@ -292,6 +290,8 @@ if(WIN32)
 	ELSE( MINGW )
 		set(SQLB_SRC ${SQLB_SRC} "${CMAKE_CURRENT_SOURCE_DIR}/src/winapp.rc")
 	ENDIF( MINGW )
+else()
+	set(LPTHREAD pthread)
 endif(WIN32)
 
 #enable version check for MacOS
@@ -376,7 +376,7 @@ endif()
 target_link_libraries(${PROJECT_NAME}
 		qhexedit
 		qcustomplot
-		pthread
+		${LPTHREAD}
 		${QT_LIBRARIES}
 		${WIN32_STATIC_LINK}
 		${LIBSQLITE}

src/ExportDataDialog.cpp
@@ -117,7 +117,7 @@ bool ExportDataDialog::exportQueryCsv(const QString& sQuery, const QString& sFil
         QByteArray utf8Query = sQuery.toUtf8();
         sqlite3_stmt *stmt;
 
-        auto pDb = pdb.get("exporting CSV");
+        auto pDb = pdb.get(tr("exporting CSV"));
         int status = sqlite3_prepare_v2(pDb.get(), utf8Query.data(), utf8Query.size(), &stmt, nullptr);
         if(SQLITE_OK == status)
         {
@@ -201,7 +201,7 @@ bool ExportDataDialog::exportQueryJson(const QString& sQuery, const QString& sFi
         QByteArray utf8Query = sQuery.toUtf8();
         sqlite3_stmt *stmt;
 
-        auto pDb = pdb.get("exporting JSON");
+        auto pDb = pdb.get(tr("exporting JSON"));
         int status = sqlite3_prepare_v2(pDb.get(), utf8Query.data(), utf8Query.size(), &stmt, nullptr);
 
         QJsonArray json_table;

src/ExtendedTableWidget.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "ExtendedTableWidget.h"
 #include "sqlitetablemodel.h"
 #include "FilterTableHeader.h"
@@ -612,8 +610,6 @@ void ExtendedTableWidget::updateGeometries()
 
 void ExtendedTableWidget::vscrollbarChanged(int value)
 {
-    //std::cout << "ExtendedTableWidget::vscrollbarChanged " << value << std::endl;
-
     // Cancel if there is no model set yet - this shouldn't happen (because without a model there should be no scrollbar) but just to be sure...
     if(!model())
         return;

src/ImportCsvDialog.cpp
@@ -607,7 +607,7 @@ bool ImportCsvDialog::importCsv(const QString& fileName, const QString& name)
     sQuery.chop(1); // Remove last comma
     sQuery.append(")");
     sqlite3_stmt* stmt;
-    auto pDb = pdb->get("importing CSV");
+    auto pDb = pdb->get(tr("importing CSV"));
     sqlite3_prepare_v2(pDb.get(), sQuery.toUtf8(), sQuery.toUtf8().length(), &stmt, nullptr);
 
     // Parse entire file

src/MainWindow.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
@@ -824,7 +822,7 @@ void MainWindow::setRecordsetLabel()
     }
     ui->labelRecordset->setText(txt);
 
-    ui->dataTable->setDisabled( m_browseTableModel->rowCountAvailable() == SqliteTableModel::RowCount::Unknown );
+    enableEditing(m_browseTableModel->rowCountAvailable() != SqliteTableModel::RowCount::Unknown, 0);
 }
 
 void MainWindow::refresh()
@@ -1152,7 +1150,7 @@ void MainWindow::executeQuery()
         // Execute next statement
         int tail_length_before = tail_length;
         const char* qbegin = tail;
-        auto pDb = db.get("executing query");
+        auto pDb = db.get(tr("executing query"));
         sql3status = sqlite3_prepare_v2(pDb.get(), tail, tail_length, &vm, &tail);
         QString queryPart = QString::fromUtf8(qbegin, tail - qbegin);
         tail_length -= (tail - qbegin);
@@ -1668,8 +1666,10 @@ void MainWindow::enableEditing(bool enable_edit, bool enable_insert)
     bool insert = enable_insert && !db.readOnly();
 
     // Apply settings
-    ui->buttonNewRecord->setEnabled(insert);
-    ui->buttonDeleteRecord->setEnabled(edit);
+    if(insert)
+        ui->buttonNewRecord->setEnabled(insert);
+    if(edit)
+        ui->buttonDeleteRecord->setEnabled(edit);
     ui->dataTable->setEditTriggers(edit ? QAbstractItemView::SelectedClicked | QAbstractItemView::AnyKeyPressed | QAbstractItemView::EditKeyPressed : QAbstractItemView::NoEditTriggers);
 }
 
@@ -2953,7 +2953,7 @@ void MainWindow::requestCollation(const QString& name, int eTextRep)
                    "If you choose to proceed, be aware bad things can happen to your database.\n"
                    "Create a backup!").arg(name), QMessageBox::Yes | QMessageBox::No);
     if(reply == QMessageBox::Yes) {
-        auto pDb = db.get("creating collation");
+        auto pDb = db.get(tr("creating collation"));
         sqlite3_create_collation(pDb.get(), name.toUtf8(), eTextRep, nullptr, collCompare);
     }
 }

src/PlotDock.cpp
@@ -424,12 +424,11 @@ void PlotDock::updatePlot(SqliteTableModel* model, BrowseDataTableSettings* sett
     ui->plotWidget->replot();
 
     // Warn user if not all data has been fetched and hint about the button for loading all the data
-    if (model->rowCountAvailable() != SqliteTableModel::RowCount::Complete || !model->isCacheComplete()) {
+    if (model && (model->rowCountAvailable() != SqliteTableModel::RowCount::Complete || !model->isCacheComplete())) {
         ui->buttonLoadAllData->setEnabled(true);
         ui->buttonLoadAllData->setStyleSheet("QToolButton {color: white; background-color: rgb(255, 102, 102)}");
         ui->buttonLoadAllData->setToolTip(tr("Load all data and redraw plot.\n"
                                              "Warning: not all data has been fetched from the table yet due to the partial fetch mechanism."));
-        QToolTip::showText(ui->buttonLoadAllData->mapToGlobal(QPoint(0, 0)), ui->buttonLoadAllData->toolTip());
     } else {
         ui->buttonLoadAllData->setEnabled(false);
         ui->buttonLoadAllData->setStyleSheet("");

src/RowLoader.cpp
@@ -201,8 +201,6 @@ void RowLoader::run ()
 
 void RowLoader::process (Task & t)
 {
-    //std::cout << "RowLoader new task: " << t.row_begin << " --> " << t.row_end << std::endl;
-
     QString sLimitQuery;
     if(query.startsWith("PRAGMA", Qt::CaseInsensitive) || query.startsWith("EXPLAIN", Qt::CaseInsensitive))
     {
@@ -254,11 +252,4 @@ void RowLoader::process (Task & t)
 
     if(row != t.row_begin)
         emit fetched(t.token, t.row_begin, row);
-
-#if 0
-    if(t.cancel)
-        std::cout << "RowLoader task was CANCELLED\n";
-    else
-        std::cout << "RowLoader task done\n";
-#endif
 }

commit e1ef492d78245daab66ccad319cbe148886d1388

Issue #1484 & #1480: crash when closing a DB having scrolled a table
Following a series of signals after closing the DB the application reaches
SqliteTableModel::isEditable() which isn't prepared for closed databases.
Taking this into account fixes the crash.

This crash seems to have being introduced in 3da520cdd1de73b09c32fe2aad08e2c3d577550b

src/sqlitetablemodel.cpp
@@ -913,7 +913,7 @@ void SqliteTableModel::setPseudoPk(const QString& pseudoPk)
 
 bool SqliteTableModel::isEditable() const
 {
-    return !m_sTable.isEmpty() && (m_db.getObjectByName(m_sTable)->type() == sqlb::Object::Types::Table || !m_pseudoPk.isEmpty());
+    return !m_sTable.isEmpty() && m_db.isOpen() && (m_db.getObjectByName(m_sTable)->type() == sqlb::Object::Types::Table || !m_pseudoPk.isEmpty());
 }
 
 void SqliteTableModel::triggerCacheLoad (int row) const