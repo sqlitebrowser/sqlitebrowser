sqlitebrowser/sqlitebrowser

commit d5a049062d4795ba91b860ac33561450ba1dce8c

Fix crash when browsing a view
This was introduced by 2b6153d9b8af5b5d806af02eb039c6a857bf0516.

src/MainWindow.cpp
@@ -3021,6 +3021,12 @@ void MainWindow::unlockViewEditing(bool unlock, QString pk)
         enableEditing(true);
         return;
     }
+
+    // If the settings didn't change, do nothing. This avoids an infinite loop
+    BrowseDataTableSettings& settings = browseTableSettings[currentTable];
+    if(unlock != settings.unlockViewPk.isEmpty() && settings.unlockViewPk == pk)
+        return;
+
     sqlb::ViewPtr obj = db.getObjectByName(currentTable).dynamicCast<sqlb::View>();
 
     // If the view gets unlocked for editing and we don't have a 'primary key' for this view yet, then ask for one
@@ -3065,7 +3071,6 @@ void MainWindow::unlockViewEditing(bool unlock, QString pk)
     ui->actionUnlockViewEditing->blockSignals(false);
 
     // Save settings for this table
-    BrowseDataTableSettings& settings = browseTableSettings[currentTable];
     settings.unlockViewPk = pk;
 
     // Reapply the view settings. This seems to be necessary as a workaround for newer Qt versions.

commit 0b8e2741c6c0e98a60fbc64ea5571c25cfa922e5

Unlock view status was lost after switching view or refresh
This solves these two problems that seem to have being introduced in
d5a049062d4795ba91b860ac33561450ba1dce8c

- The unlocked-view status was not applied again when switching back to the
view (you cannot edit and the Delete Record button is not reenabled) or
after refreshing.

- The 'Unlock view editing' check state persisted when switching between
views.

See issue #141.

src/MainWindow.cpp
@@ -3182,11 +3182,6 @@ void MainWindow::unlockViewEditing(bool unlock, QString pk)
         return;
     }
 
-    // If the settings didn't change, do nothing. This avoids an infinite loop
-    BrowseDataTableSettings& settings = browseTableSettings[currentTable];
-    if(unlock != settings.unlockViewPk.isEmpty() && settings.unlockViewPk == pk)
-        return;
-
     sqlb::ViewPtr obj = db.getObjectByName<sqlb::View>(currentTable);
 
     // If the view gets unlocked for editing and we don't have a 'primary key' for this view yet, then ask for one
@@ -3230,11 +3225,16 @@ void MainWindow::unlockViewEditing(bool unlock, QString pk)
     ui->actionUnlockViewEditing->setChecked(unlock);
     ui->actionUnlockViewEditing->blockSignals(false);
 
-    // Save settings for this table
-    settings.unlockViewPk = pk;
+    // If the settings didn't change, do not try to reapply them.
+    // This avoids an infinite mutual recursion.
+    BrowseDataTableSettings& settings = browseTableSettings[currentTable];
 
-    // Reapply the view settings. This seems to be necessary as a workaround for newer Qt versions.
-    applyBrowseTableSettings(settings);
+    if(settings.unlockViewPk != pk) {
+        // Save settings for this table
+        settings.unlockViewPk = pk;
+        // Reapply the view settings. This seems to be necessary as a workaround for newer Qt versions.
+        applyBrowseTableSettings(settings);
+    }
 }
 
 sqlb::ObjectIdentifier MainWindow::currentlyBrowsedTableName() const