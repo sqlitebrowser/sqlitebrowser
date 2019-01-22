1efaa6d55daf6e012882c539a057ca275f9a9faf
Paul Jarvis <apjarvis@gmail.com>
2019-01-22 09:26:01 +0000

    Added the ability to decode base64 data. Added the ability to decode 'plist' data structures.
    Additional code is in C (not C++) so really needs re-working
    but only if of wider interest.

--- src/EditDialog.h
+++ src/EditDialog.h
@@ -74,7 +74,8 @@ private:
         Null,
         Text,
         JSON,
-        SVG
+        SVG,
+        Plist
     };
 
     // Edit modes and editor stack (this must be aligned with the UI)
@@ -84,7 +85,8 @@ private:
         HexEditor = 1,
         ImageViewer = 2,
         JsonEditor, SciEditor = 3,
-        XmlEditor = 4
+        XmlEditor = 4,
+        Base64Editor = 5
     };
 
     int checkDataType(const QByteArray& data);
