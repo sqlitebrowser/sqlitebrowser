f7a657492a8d2df6a61a36a31e3ff9dc605ea890
Paul Jarvis <apjarvis@gmail.com>
2019-02-02 10:31:25 +0000

    Correction to header length test

--- src/extensions/extension-plist.c
+++ src/extensions/extension-plist.c
@@ -110,9 +110,9 @@ int printWithIndent(CONFIG *cfg, const char *text, int newline)
 
 int readHeader(CONFIG *cfg)
 {
-  if ((cfg->bufferLength > 40) &&
-     ((strncmp((const char *)(cfg->buffer), "bplist0", 7) != 0) ||
-     ((cfg->buffer[7] != '0') && (cfg->buffer[7] != '1'))))
+  if ((cfg->bufferLength < 400) ||
+      (strncmp((const char *)(cfg->buffer), "bplist0", 7) != 0) ||
+      ((cfg->buffer[7] != '0') && (cfg->buffer[7] != '1')))
     return ERROR_INVALID_HEADER;
   return ERROR_NONE;
 }
