180fb51ae27494ff4e29f5e1ad57581ffe9a40f3
Martin Kleusberg <mkleusberg@gmail.com>
2019-02-14 18:01:20 +0100

    Further performance improvements in the SQL import
    This eliminates some unneeded conversions in the SQL import code which
    should speed up the import and reduce its memory consumption.
    
    See issue #1045.

--- src/sqlitedb.h
+++ src/sqlitedb.h
@@ -95,7 +95,7 @@ public:
     bool dump(const QString& filename, const QStringList& tablesToDump, bool insertColNames, bool insertNew, bool exportSchema, bool exportData, bool keepOldSchema);
 
     bool executeSQL(QString statement, bool dirtyDB = true, bool logsql = true);
-    bool executeMultiSQL(QString query, bool dirty = true, bool log = false);
+    bool executeMultiSQL(QByteArray query, bool dirty = true, bool log = false);
     QVariant querySingleValueFromDb(const QString& statement, bool log = true);
 
     const QString& lastError() const { return lastErrorMessage; }
