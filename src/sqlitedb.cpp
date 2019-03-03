sqlitebrowser/sqlitebrowser

commit 0f6946c19dacb4900336bc3d043f70a3608501f7

Fix transaction detection in SQL import
Fix a couple of problems in the detection of transaction statements
during the SQL import. Before this the detection was case-dependent as
well as dependent on the number of spaces you use. Also it did not
detect 'END TRANSACTION', 'COMMIT TRANSACTION', or 'BEGIN <qualifier>
TRANSACTION' at all. Finally, it could modify your statements if you
embed string in them which look like transaction statements. All of this
is, hopefully, fixed in this commit.

See issue #1764.

src/sqlitedb.cpp
@@ -951,31 +951,9 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
         return false;
     }
 
-    // Check if this SQL containts any transaction statements. If so remove them and create a savepoint instead by overriding the dirty parameter.
-    // TODO This should check for 'END TRANSACTION' too. It should be case insensitive and it should work with any amounts of whitespace etc. "Good" news
-    // is that none of this was ever done correctly before.
-    if(query.contains("BEGIN TRANSACTION;"))
-    {
-        query.replace("BEGIN TRANSACTION;", "                  ");
-        dirty = true;
-    }
-    if(query.contains("COMMIT;"))
-    {
-        query.replace("COMMIT;", "       ");
-        dirty = true;
-    }
-
     // Log the statement if needed
     if(log)
         logSQL(query, kLogMsg_App);
-
-    // Set DB to dirty/create restore point if necessary
-    QString savepoint_name;
-    if(dirty)
-    {
-        savepoint_name = generateSavepointName("execmultisql");
-        setSavepoint(savepoint_name);
-    }
 
     // Show progress dialog
     QProgressDialog progress(tr("Executing SQL..."),
@@ -993,6 +971,7 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
     unsigned int line = 0;
     bool structure_updated = false;
     int last_progress_value = -1;
+    QString savepoint_name;
     while(tail && *tail != 0 && (res == SQLITE_OK || res == SQLITE_DONE))
     {
         line++;
@@ -1031,6 +1010,25 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
                     next_statement.compare(0, 4, "DROP") == 0 ||
                     next_statement.compare(0, 8, "ROLLBACK") == 0)
                 structure_updated = true;
+
+            // Check for transaction statements and skip until the next semicolon
+            if(next_statement.compare(0, 5, "COMMIT") == 0 ||
+                    next_statement.compare(0, 4, "END ") == 0 ||
+                    next_statement.compare(0, 6, "BEGIN ") == 0)
+            {
+                while(tail != tail_end)
+                {
+                    if(*++tail == ';')
+                        break;
+                }
+
+                // Set DB to dirty and create a restore point if we haven't done that yet
+                if(dirty && savepoint_name.isNull())
+                {
+                    savepoint_name = generateSavepointName("execmultisql");
+                    setSavepoint(savepoint_name);
+                }
+            }
         }
 
         // Execute next statement

commit fa203c4c61fb12f70035771290c3484143543d0f

Fix multiple problems in 0f6946c19dacb4900336bc3d043f70a3608501f7
Fix multiple problems which were introduced in commit
0f6946c19dacb4900336bc3d043f70a3608501f7. They all are about detection
of transaction statements. First it turned out that the commit slightly
changed the behaviour of the dirty parameter which, although there did
not seem to be any consequences of this, was turned back into the old
behaviour. Then the detection of transaction statements depended on the
structure updated flag which is totally wrong and needed to be fixed.
Next, it turned out that SQLite executed ";COMMIT;" as one statement
instead of two. This means we need to skip until after the first
semicolon and not until before it. And finally, after skipping until
after the semicolon we should perform the check for transaction
statements again before blindly giving the next statement to SQLite.

See issue #1777.

src/sqlitedb.cpp
@@ -990,8 +990,7 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
             last_progress_value = progress_value;
         }
 
-        // Check whether the DB structure is changed by this statement
-        if(!dontCheckForStructureUpdates && !structure_updated)
+        // Check next statement
         {
             // Ignore all whitespace at the start of the current tail
             const char* tail_ptr = tail;
@@ -1004,13 +1003,6 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
             std::string next_statement(tail_ptr, length);
             std::transform(next_statement.begin(), next_statement.end(), next_statement.begin(), ::toupper);
 
-            // Check if it's a modifying statement
-            if(next_statement.compare(0, 5, "ALTER") == 0 ||
-                    next_statement.compare(0, 6, "CREATE") == 0 ||
-                    next_statement.compare(0, 4, "DROP") == 0 ||
-                    next_statement.compare(0, 8, "ROLLBACK") == 0)
-                structure_updated = true;
-
             // Check for transaction statements and skip until the next semicolon
             if(next_statement.compare(0, 6, "COMMIT") == 0 ||
                     next_statement.compare(0, 4, "END ") == 0 ||
@@ -1018,16 +1010,31 @@ bool DBBrowserDB::executeMultiSQL(QByteArray query, bool dirty, bool log)
             {
                 while(tail != tail_end)
                 {
-                    if(*++tail == ';')
+                    if(*tail++ == ';')
                         break;
                 }
 
                 // Set DB to dirty and create a restore point if we haven't done that yet
-                if(dirty && savepoint_name.isNull())
+                if(savepoint_name.isNull())
                 {
                     savepoint_name = generateSavepointName("execmultisql");
                     setSavepoint(savepoint_name);
+                    dirty = true;
                 }
+
+                // Don't just execute next statement. Start next statement with the same checks
+                continue;
+            }
+
+            // Check whether the DB structure is changed by this statement
+            if(!dontCheckForStructureUpdates && !structure_updated)
+            {
+                // Check if it's a modifying statement
+                if(next_statement.compare(0, 5, "ALTER") == 0 ||
+                        next_statement.compare(0, 6, "CREATE") == 0 ||
+                        next_statement.compare(0, 4, "DROP") == 0 ||
+                        next_statement.compare(0, 8, "ROLLBACK") == 0)
+                    structure_updated = true;
             }
         }
 