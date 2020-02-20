sqlitebrowser/sqlitebrowser

commit cee3523f37bf89780e42fc65fd327c2e1ea3a752

Fix the recordset numbers in the Browse Data tab
This improves the accuracy of the number of the last row in the
recordset number label at the bottom of the Browse Data tab by using the
seemingly more accurate visualIndexAt approach instead of the rowAt
approach from before and by taking the horizontal scrollbar into
account.

It also updates the label when resizing the row heights or column
widths.

See issue #232.

src/TableBrowser.cpp
@@ -130,6 +130,8 @@ TableBrowser::TableBrowser(QWidget* parent) :
     connect(ui->dataTable->filterHeader(), &FilterTableHeader::sectionClicked, this, &TableBrowser::headerClicked);
     connect(ui->dataTable->filterHeader(), &QHeaderView::sectionDoubleClicked, ui->dataTable, &QTableView::selectColumn);
     connect(ui->dataTable->verticalScrollBar(), &QScrollBar::valueChanged, this, &TableBrowser::updateRecordsetLabel);
+    connect(ui->dataTable->horizontalHeader(), &QHeaderView::sectionResized, this, &TableBrowser::updateRecordsetLabel);
+    connect(ui->dataTable->verticalHeader(), &QHeaderView::sectionResized, this, &TableBrowser::updateRecordsetLabel);
     connect(ui->dataTable->horizontalHeader(), &QHeaderView::sectionResized, this, &TableBrowser::updateColumnWidth);
     connect(ui->dataTable->horizontalHeader(), &QHeaderView::customContextMenuRequested, this, &TableBrowser::showDataColumnPopupMenu);
     connect(ui->dataTable->verticalHeader(), &QHeaderView::customContextMenuRequested, this, &TableBrowser::showRecordPopupMenu);
@@ -714,9 +716,7 @@ void TableBrowser::updateRecordsetLabel()
     // Get all the numbers, i.e. the number of the first row and the last row as well as the total number of rows
     int from = ui->dataTable->verticalHeader()->visualIndexAt(0) + 1;
     int total = m_model->rowCount();
-    int to = ui->dataTable->verticalHeader()->visualIndexAt(ui->dataTable->height()) - 1;
-    if (to == -2)
-        to = total;
+    int to = from + ui->dataTable->numVisibleRows() - 1;
 
     // Update the validator of the goto row field
     gotoValidator->setRange(0, total);

src/ExtendedTableWidget.cpp
@@ -869,9 +869,14 @@ void ExtendedTableWidget::vscrollbarChanged(int value)
 
 int ExtendedTableWidget::numVisibleRows() const
 {
+    if(!isVisible())
+        return 0;
+
     // Get the row numbers of the rows currently visible at the top and the bottom of the widget
     int row_top = rowAt(0) == -1 ? 0 : rowAt(0);
-    int row_bottom = rowAt(height()) == -1 ? model()->rowCount() : rowAt(height());
+    int row_bottom = verticalHeader()->visualIndexAt(height()) == -1 ? model()->rowCount() : (verticalHeader()->visualIndexAt(height()) - 1);
+    if(horizontalScrollBar()->isVisible())      // Assume the scrollbar covers about one row
+        row_bottom--;
 
     // Calculate the number of visible rows
     return row_bottom - row_top;

commit 13742c4602397edad2717c275a373080dc32386d

Fix recordset number in Browse Data tab showing -1
This fixes an issue earlier introduced in commit
cee3523f37bf89780e42fc65fd327c2e1ea3a752 which made the number of the
last visible record in an empty table -1 instead of 0.

src/TableBrowser.cpp
@@ -717,6 +717,8 @@ void TableBrowser::updateRecordsetLabel()
     int from = ui->dataTable->verticalHeader()->visualIndexAt(0) + 1;
     int total = m_model->rowCount();
     int to = from + ui->dataTable->numVisibleRows() - 1;
+    if(to < 0)
+            to = 0;
 
     // Update the validator of the goto row field
     gotoValidator->setRange(0, total);