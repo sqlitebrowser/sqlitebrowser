862fe01d6bd4f7609e661e4f2590a1c0111a76a9
Justin Clift <justin@postgresql.org>
2020-06-06 13:33:51 +1000

    Update Windows icon creation script at Scott suggestion :)
    As per https://github.com/sqlitebrowser/sqlitebrowser/pull/2270

--- src/tools/create_windows_icon.sh
+++ src/tools/create_windows_icon.sh
@@ -4,7 +4,7 @@
 
 for imgsize in 16 32 64 128
 do
-	convert ../icons/sqlitebrowser.png -resize "$imgsize"x"$imgsize" -background transparent icon"$imgsize".png
+	convert ../../images/logo.svg -resize "$imgsize"x"$imgsize" -background transparent icon"$imgsize".png
 	files="$files icon$imgsize.png"
 done
 
