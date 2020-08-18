ed226f1d0a6e816e2ffbd0ab8df04e888459f519
mgrojo <mgrojo@gmail.com>
2020-08-18 21:56:15 +0200

    Add file, line number and function to Error Log
    This will be useful for debugging purposes. Note that sqlite3_log has a
    limit and the final text could be truncated.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include "Application.h"
 #include "sqlite.h"
 
-void db4sMessageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg)
+void db4sMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     QByteArray localMsg = msg.toLocal8Bit();
 
     // Emulate the default Qt treatment. This might not work in some OS, like Windows.
     fprintf(stderr, "%s\n", localMsg.constData());
 
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    localMsg = QString("%1 (%2, %3:%4)\n").arg(msg).arg(function).arg(file).arg(context.line).toLocal8Bit();
+
     // Log using the SQLite log mechanism, so it gets the same treatment than messages
     // reported by SQLite itself. This will allow these messages to be seen in our Log window.
     // Error code will be the type
-    sqlite3_log(static_cast<int>(type), "%s\n", localMsg.constData());
+    sqlite3_log(static_cast<int>(type), localMsg.constData());
 }
 
 int main( int argc, char ** argv )
