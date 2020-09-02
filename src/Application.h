3f35595b340b84d09515350c084d28371603ee9d
mgrojo <mgrojo@gmail.com>
2020-09-02 21:35:37 +0200

    Unify message in About dialog and version argument
    And avoid removing extension when printing command name in help message.
    
    See issue #1069

--- src/Application.h
+++ src/Application.h
@@ -21,7 +21,10 @@ public:
 
     MainWindow* mainWindow() { return m_mainWindow; }
 
+    // DB4S version number as string
     static QString versionString();
+    // Version of DB4S and dependencies as string
+    static QString versionInformation();
 
     static void reloadSettings();
 
