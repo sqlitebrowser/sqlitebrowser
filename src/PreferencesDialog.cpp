9b387d2061f94ad9fb3811aa58e590e2087a6c1c
Martin Kleusberg <github@mkleusberg.de>
2020-11-25 23:18:52 +0100

    Fix saving the list of extensions in the Preferences dialog
    Thanks to @sandman7920 for providing the analysis and fix.
    
    See issue #2467.

--- src/PreferencesDialog.cpp
+++ src/PreferencesDialog.cpp
@@ -267,8 +267,8 @@ void PreferencesDialog::saveSettings(bool accept)
     Settings::setValue("editor", "close_button_on_tabs", ui->checkCloseButtonOnTabs->isChecked());
 
     QStringList extList;
-    for(const QListWidgetItem* item : ui->listExtensions->findItems(QString("*"), Qt::MatchWrap | Qt::MatchWildcard))
-        extList.append(item->text());
+    for(int i=0;i<ui->listExtensions->count();++i)
+        extList.append(ui->listExtensions->item(i)->text());
     Settings::setValue("extensions", "list", extList);
     Settings::setValue("extensions", "disableregex", ui->checkRegexDisabled->isChecked());
     Settings::setValue("extensions", "enable_load_extension", ui->checkAllowLoadExtension->isChecked());
