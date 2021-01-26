sqlitebrowser/sqlitebrowser

commit e54664dd03ccda5e3ca4a0056a3f7229f1d46f25

Change some function parameters with negated name
Change some function parameters which are named like "dont..." to make a
positive statement. This should hopefully avoid some confisions with
double negation.

src/Settings.cpp
@@ -104,11 +104,11 @@ QVariant Settings::getValue(const std::string& group, const std::string& name)
     }
 }
 
-void Settings::setValue(const std::string& group, const std::string& name, const QVariant& value, bool dont_save_to_disk)
+void Settings::setValue(const std::string& group, const std::string& name, const QVariant& value, bool save_to_disk)
 {
     // Sometime the value has to be saved for the current session only but get discarded when the application exits.
     // In order to achieve this this flag can be set which disables the save to disk mechanism and only leaves the save to cache part active.
-    if(dont_save_to_disk == false)
+    if(save_to_disk == false)
     {
         setSettingsObject();
         // Set the group and save the given value

src/Application.cpp
@@ -106,7 +106,7 @@ Application::Application(int& argc, char** argv) :
     std::vector<QString> tableToBrowse;
     QStringList sqlToExecute;
     bool readOnly = false;
-    m_dontShowMainWindow = false;
+    m_showMainWindow = true;
     for(int i=1;i<arguments().size();i++)
     {
         // Check next command line argument
@@ -130,10 +130,10 @@ Application::Application(int& argc, char** argv) :
             qWarning() << qPrintable(tr("  -v, --version       Display the current version"));
             qWarning() << qPrintable(tr("  <database>          Open this SQLite database"));
             qWarning() << qPrintable(tr("  <project>           Open this project file (*.sqbpro)"));
-            m_dontShowMainWindow = true;
+            m_showMainWindow = false;
         } else if(arguments().at(i) == "-v" || arguments().at(i) == "--version") {
             qWarning() << qPrintable(versionInformation());
-            m_dontShowMainWindow = true;
+            m_showMainWindow = false;
         } else if(arguments().at(i) == "-s" || arguments().at(i) == "--sql") {
             // Run SQL file: If file exists add it to list of scripts to execute
             if(++i >= arguments().size())
@@ -148,7 +148,7 @@ Application::Application(int& argc, char** argv) :
             else
                 tableToBrowse.push_back(arguments().at(i));
         } else if(arguments().at(i) == "-q" || arguments().at(i) == "--quit") {
-            m_dontShowMainWindow = true;
+            m_showMainWindow = false;
         } else if(arguments().at(i) == "-R" || arguments().at(i) == "--read-only") {
             readOnly = true;
         } else if(arguments().at(i) == "-S" || arguments().at(i) == "--settings") {
@@ -177,7 +177,7 @@ Application::Application(int& argc, char** argv) :
                             value = option.at(1).split(",");
                         else
                             value = option.at(1);
-                        Settings::setValue(setting.at(0).toStdString(), setting.at(1).toStdString(), value, !saveToDisk);
+                        Settings::setValue(setting.at(0).toStdString(), setting.at(1).toStdString(), value, saveToDisk);
                     }
                 }
             }
@@ -190,7 +190,7 @@ Application::Application(int& argc, char** argv) :
         }
     }
 
-    if(m_dontShowMainWindow) {
+    if(!m_showMainWindow) {
         m_mainWindow = nullptr;
         return;
     }

src/Application.h
@@ -17,7 +17,7 @@ public:
     explicit Application(int& argc, char** argv);
     ~Application() override;
 
-    bool dontShowMainWindow() const { return m_dontShowMainWindow; }
+    bool showMainWindow() const { return m_showMainWindow; }
 
     MainWindow* mainWindow() { return m_mainWindow; }
 
@@ -32,7 +32,7 @@ protected:
     bool event(QEvent* event) override;
 
 private:
-    bool m_dontShowMainWindow;
+    bool m_showMainWindow;
     MainWindow* m_mainWindow;
     QTranslator* m_translatorQt;
     QTranslator* m_translatorApp;

src/FileDialog.cpp
@@ -70,7 +70,7 @@ void FileDialog::setFileDialogPath(const FileDialogTypes dialogType, const QStri
         Settings::setValue("db", "lastlocations", lastLocations);
         break;
     case 2:     // Remember last location for current session only
-        Settings::setValue("db", "lastlocations", lastLocations, true);
+        Settings::setValue("db", "lastlocations", lastLocations, false);
         break;
     case 1:     // Always use this locations
         break;  // Do nothing

src/MainWindow.cpp
@@ -2791,7 +2791,7 @@ bool MainWindow::loadProject(QString filename, bool readOnly)
                                                   "it completely, please use DB Browser for SQLite version 3.12 to convert it to the new file format."));
                                 msgBox.exec();
                                 if(msgBox.clickedButton() == idontcarebutton)
-                                    Settings::setValue("idontcare", "projectBrowseTable", true);
+                                    Settings::setValue("idontcare", "projectBrowseTable", false);
                             }
 
                             xml.skipCurrentElement();
@@ -3334,7 +3334,7 @@ void MainWindow::requestCollation(const QString& name, int eTextRep)
         // Cancel here if the No button was clicked
         if(msgbox.clickedButton() == button_dont_ask_again)
         {
-            Settings::setValue("db", "dont_ask_collation", true);
+            Settings::setValue("db", "dont_ask_collation", false);
         } else if(reply == QMessageBox::No) {
             return;
         }

src/Settings.h
@@ -17,7 +17,7 @@ public:
 
     static void setUserSettingsFile(const QString& userSettingsFileArg);
     static QVariant getValue(const std::string& group, const std::string& name);
-    static void setValue(const std::string& group, const std::string& name, const QVariant& value, bool dont_save_to_disk = false);
+    static void setValue(const std::string& group, const std::string& name, const QVariant& value, bool save_to_disk = true);
     static void clearValue(const std::string& group, const std::string& name);
     static void restoreDefaults();
 

src/SqlExecutionArea.cpp
@@ -44,10 +44,10 @@ SqlExecutionArea::SqlExecutionArea(DBBrowserDB& _db, QWidget* parent) :
 
     // Save to settings when sppliter is moved, but only to memory.
     connect(ui->splitter, &QSplitter::splitterMoved, this,  [this]() {
-            Settings::setValue("editor", "splitter1_sizes", ui->splitter->saveState(), /* dont_save_to_disk */ true);
+            Settings::setValue("editor", "splitter1_sizes", ui->splitter->saveState(), /* save_to_disk */ false);
         });
     connect(ui->splitter_2, &QSplitter::splitterMoved, this, [this]() {
-            Settings::setValue("editor", "splitter2_sizes", ui->splitter_2->saveState(), /* dont_save_to_disk */ true);
+            Settings::setValue("editor", "splitter2_sizes", ui->splitter_2->saveState(), /* save_to_disk */ false);
         });
 
     // Set collapsible the editErrors panel

src/main.cpp
@@ -56,7 +56,7 @@ int main( int argc, char ** argv )
     }
 
     // Quit application now if user doesn't want to see the UI
-    if(a.dontShowMainWindow())
+    if(!a.showMainWindow())
         return 0;
 
     qInstallMessageHandler(db4sMessageOutput);

commit f6ebc74131735d8ada544e612db24b3594ec0c21

Fix e54664dd03ccda5e3ca4a0056a3f7229f1d46f25
Fix an error introduced in e54664dd03ccda5e3ca4a0056a3f7229f1d46f25.

src/Settings.cpp
@@ -108,7 +108,7 @@ void Settings::setValue(const std::string& group, const std::string& name, const
 {
     // Sometime the value has to be saved for the current session only but get discarded when the application exits.
     // In order to achieve this this flag can be set which disables the save to disk mechanism and only leaves the save to cache part active.
-    if(save_to_disk == false)
+    if(save_to_disk)
     {
         setSettingsObject();
         // Set the group and save the given value