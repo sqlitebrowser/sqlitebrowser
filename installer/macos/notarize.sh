03bbbc61594620432b5f69136b2b3e9f12ada866
SeongTae Jeong <seongtaejg@gmail.com>
2024-09-11 20:04:08 +0900

    Prepare to build v3.13.1-rc1

--- installer/macos/notarize.sh
+++ installer/macos/notarize.sh
@@ -74,7 +74,7 @@ done
 mv build/*.app installer/macos
 
 # Create the DMG
-TARGET="DB.Browser.for.SQLite-v3.13.0.dmg"
+TARGET="DB.Browser.for.SQLite-v3.13.1-rc1.dmg"
 appdmg --quiet installer/macos/nightly.json "$TARGET"
 
 codesign --sign "$DEV_ID" --verbose --options=runtime --timestamp "$TARGET"
