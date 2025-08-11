defaa47a3d562e999f24d7f861a8dc8bb017e015
12private12 <120792mon@gmail.com>
2025-08-12 00:32:23 +0800

    Delete config directory

--- config/options.cmake
+++
@@ -1,9 +0,0 @@
-set(QT_MAJOR Qt5 CACHE STRING "Major QT version")
-OPTION(BUILD_STABLE_VERSION "Don't build the stable version by default" OFF) # Choose between building a stable version or nightly (the default), depending on whether '-DBUILD_STABLE_VERSION=1' is passed on the command line or not.
-OPTION(ENABLE_TESTING "Enable the unit tests" OFF)
-OPTION(FORCE_INTERNAL_QSCINTILLA "Don't use the distribution's QScintilla library even if there is one" OFF)
-OPTION(FORCE_INTERNAL_QCUSTOMPLOT "Don't use distribution's QCustomPlot even if available" ON)
-OPTION(FORCE_INTERNAL_QHEXEDIT "Don't use distribution's QHexEdit even if available" ON)
-OPTION(ALL_WARNINGS "Enable some useful warning flags" OFF)
-OPTION(sqlcipher "Build with SQLCipher library" OFF)
-OPTION(customTap "Using SQLCipher, SQLite and Qt installed through our custom Homebrew tap" OFF)
